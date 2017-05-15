7b97e77b83de358a48eb15587430c1cb8797c879
zhi_heart <zhi_heart@aliyun.com>
2017-05-15 16:21:04 +0800

    ik动态词库

--- collect/src/main/java/com/yanerwu/action/OpenAction.java
+++ collect/src/main/java/com/yanerwu/action/OpenAction.java
@@ -2,6 +2,7 @@ package com.yanerwu.action;
 
 import com.yanerwu.Cache;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,6 +15,7 @@ import java.util.Date;
  * @Date 2017/5/15 15:10
  * @Description
  */
+@Controller
 public class OpenAction {
 
     @Autowired
