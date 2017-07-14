e90edaa87e2628c8e890897bdc4b1de376c07329
zhi_heart <zhi_heart@aliyun.com>
2017-07-14 10:23:31 +0800

    小说wap版

--- service/src/main/java/com/yanerwu/entity/BookSummary.java
+++ service/src/main/java/com/yanerwu/entity/BookSummary.java
@@ -16,175 +16,201 @@ import org.apache.commons.lang3.builder.ToStringStyle;
  */
 @Table(name="book_summary")
 public class BookSummary extends BaseEntity implements java.io.Serializable{
-    private static final long serialVersionUID = 5454155825314635342L;
+	private static final long serialVersionUID = 5454155825314635342L;
 
-    //alias
-    public static final String TABLE_ALIAS = "BookSummary";
-    public static final String ALIAS_ID = "id";
-    public static final String ALIAS_QIDIAN_ID = "qidianId";
-    public static final String ALIAS_NAME = "name";
-    public static final String ALIAS_TYPE_ID = "typeId";
-    public static final String ALIAS_TYPE = "type";
-    public static final String ALIAS_STATUS = "status";
-    public static final String ALIAS_SUMMARY = "summary";
-    public static final String ALIAS_AUTHOR = "author";
-    public static final String ALIAS_CREATE_TIME = "createTime";
-    public static final String ALIAS_UPDATE_TIME = "updateTime";
+	//alias
+	public static final String TABLE_ALIAS = "BookSummary";
+	public static final String ALIAS_ID = "id";
+	public static final String ALIAS_QIDIAN_ID = "qidianId";
+	public static final String ALIAS_QIDIAN_URL = "qidianUrl";
+	public static final String ALIAS_BIQUGE_ID = "biqugeId";
+	public static final String ALIAS_BIQUGE_URL = "biqugeUrl";
+	public static final String ALIAS_NAME = "name";
+	public static final String ALIAS_TYPE_ID = "typeId";
+	public static final String ALIAS_TYPE = "type";
+	public static final String ALIAS_STATUS = "status";
+	public static final String ALIAS_SUMMARY = "summary";
+	public static final String ALIAS_AUTHOR = "author";
+	public static final String ALIAS_CREATE_TIME = "createTime";
+	public static final String ALIAS_UPDATE_TIME = "updateTime";
 
-    //date formats
+	//date formats
 
-    //columns START
+	//columns START
     /**序号*/
-    @Id
-    @Column(name="id")
-    private java.lang.Long id;
-    /***/
-    @Column(name="qidian_id")
-    private java.lang.Integer qidianId;
+	@Id
+	@Column(name="id")
+	private Long id;
+    /**起点序号*/
+	@Column(name="qidian_id")
+	private Integer qidianId;
+    /**起点链接*/
+	@Column(name="qidian_url")
+	private String qidianUrl;
+    /**笔趣阁序号*/
+	@Column(name="biquge_id")
+	private Integer biqugeId;
+    /**笔趣阁链接*/
+	@Column(name="biquge_url")
+	private String biqugeUrl;
     /**书名*/
-    @Column(name="name")
-    private java.lang.String name;
+	@Column(name="name")
+	private String name;
     /**类别?{"0":"未知","1":"武侠"}*/
-    @Column(name="type_id")
-    private java.lang.Integer typeId;
-    /***/
-    @Column(name="type")
-    private java.lang.String type;
+	@Column(name="type_id")
+	private Integer typeId;
+    /**类别*/
+	@Column(name="type")
+	private String type;
     /**状态?{"0":"连载","1":"全本","2":"未知"}*/
-    @Column(name="status")
-    private java.lang.Integer status;
+	@Column(name="status")
+	private Integer status;
     /**简介*/
-    @Column(name="summary")
-    private java.lang.String summary;
+	@Column(name="summary")
+	private String summary;
     /**作者*/
-    @Column(name="author")
-    private java.lang.String author;
+	@Column(name="author")
+	private String author;
     /**创建时间*/
-    @Column(name="create_time")
-    private String createTime;
+	@Column(name="create_time")
+	private String createTime;
     /**更新时间*/
-    @Column(name="update_time")
-    private String updateTime;
-    //columns END
-    @Column(name="biquge_url")
-    private String biqugeUrl;
+	@Column(name="update_time")
+	private String updateTime;
+	//columns END
 
-    public BookSummary(){
-    }
+	public BookSummary(){
+	}
 
-    public BookSummary(
-            java.lang.Long id
-    ){
-        this.id = id;
-    }
+	public BookSummary(
+		Long id
+	){
+		this.id = id;
+	}
 
-    public void setId(java.lang.Long value) {
-        this.id = value;
-    }
+	public void setId(Long value) {
+		this.id = value;
+	}
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    public void setQidianId(java.lang.Integer value) {
-        this.qidianId = value;
-    }
+	public Long getId() {
+		return this.id;
+	}
+	public void setQidianId(Integer value) {
+		this.qidianId = value;
+	}
 
-    public java.lang.Integer getQidianId() {
-        return this.qidianId;
-    }
-    public void setName(java.lang.String value) {
-        this.name = value;
-    }
+	public Integer getQidianId() {
+		return this.qidianId;
+	}
+	public void setQidianUrl(String value) {
+		this.qidianUrl = value;
+	}
 
-    public java.lang.String getName() {
-        return this.name;
-    }
-    public void setTypeId(java.lang.Integer value) {
-        this.typeId = value;
-    }
+	public String getQidianUrl() {
+		return this.qidianUrl;
+	}
+	public void setBiqugeId(Integer value) {
+		this.biqugeId = value;
+	}
 
-    public java.lang.Integer getTypeId() {
-        return this.typeId;
-    }
-    public void setType(java.lang.String value) {
-        this.type = value;
-    }
+	public Integer getBiqugeId() {
+		return this.biqugeId;
+	}
+	public void setBiqugeUrl(String value) {
+		this.biqugeUrl = value;
+	}
 
-    public java.lang.String getType() {
-        return this.type;
-    }
-    public void setStatus(java.lang.Integer value) {
-        this.status = value;
-    }
+	public String getBiqugeUrl() {
+		return this.biqugeUrl;
+	}
+	public void setName(String value) {
+		this.name = value;
+	}
 
-    public java.lang.Integer getStatus() {
-        return this.status;
-    }
-    public void setSummary(java.lang.String value) {
-        this.summary = value;
-    }
+	public String getName() {
+		return this.name;
+	}
+	public void setTypeId(Integer value) {
+		this.typeId = value;
+	}
 
-    public java.lang.String getSummary() {
-        return this.summary;
-    }
-    public void setAuthor(java.lang.String value) {
-        this.author = value;
-    }
+	public Integer getTypeId() {
+		return this.typeId;
+	}
+	public void setType(String value) {
+		this.type = value;
+	}
 
-    public java.lang.String getAuthor() {
-        return this.author;
-    }
-    public void setCreateTime(String value) {
-        this.createTime = value;
-    }
+	public String getType() {
+		return this.type;
+	}
+	public void setStatus(Integer value) {
+		this.status = value;
+	}
 
-    public String getCreateTime() {
-        return this.createTime;
-    }
-    public void setUpdateTime(String value) {
-        this.updateTime = value;
-    }
+	public Integer getStatus() {
+		return this.status;
+	}
+	public void setSummary(String value) {
+		this.summary = value;
+	}
 
-    public String getUpdateTime() {
-        return this.updateTime;
-    }
+	public String getSummary() {
+		return this.summary;
+	}
+	public void setAuthor(String value) {
+		this.author = value;
+	}
 
-    public String getBiqugeUrl() {
-        return biqugeUrl;
-    }
+	public String getAuthor() {
+		return this.author;
+	}
+	public void setCreateTime(String value) {
+		this.createTime = value;
+	}
 
-    public void setBiqugeUrl(String biqugeUrl) {
-        this.biqugeUrl = biqugeUrl;
-    }
+	public String getCreateTime() {
+		return this.createTime;
+	}
+	public void setUpdateTime(String value) {
+		this.updateTime = value;
+	}
 
-    public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-                .append("Id",getId())
-                .append("QidianId",getQidianId())
-                .append("Name",getName())
-                .append("TypeId",getTypeId())
-                .append("Type",getType())
-                .append("Status",getStatus())
-                .append("Summary",getSummary())
-                .append("Author",getAuthor())
-                .append("CreateTime",getCreateTime())
-                .append("UpdateTime",getUpdateTime())
-                .toString();
-    }
+	public String getUpdateTime() {
+		return this.updateTime;
+	}
 
-    public int hashCode() {
-        return new HashCodeBuilder()
-                .append(getId())
-                .toHashCode();
-    }
+	public String toString() {
+		return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+			.append("Id",getId())
+			.append("QidianId",getQidianId())
+			.append("QidianUrl",getQidianUrl())
+			.append("BiqugeId",getBiqugeId())
+			.append("BiqugeUrl",getBiqugeUrl())
+			.append("Name",getName())
+			.append("TypeId",getTypeId())
+			.append("Type",getType())
+			.append("Status",getStatus())
+			.append("Summary",getSummary())
+			.append("Author",getAuthor())
+			.append("CreateTime",getCreateTime())
+			.append("UpdateTime",getUpdateTime())
+			.toString();
+	}
 
-    public boolean equals(Object obj) {
-        if(obj instanceof BookSummary == false) return false;
-        if(this == obj) return true;
-        BookSummary other = (BookSummary)obj;
-        return new EqualsBuilder()
-                .append(getId(),other.getId())
-                .isEquals();
-    }
+	public int hashCode() {
+		return new HashCodeBuilder()
+			.append(getId())
+			.toHashCode();
+	}
+
+	public boolean equals(Object obj) {
+		if(obj instanceof BookSummary == false) return false;
+		if(this == obj) return true;
+		BookSummary other = (BookSummary)obj;
+		return new EqualsBuilder()
+			.append(getId(),other.getId())
+			.isEquals();
+	}
 }
 
