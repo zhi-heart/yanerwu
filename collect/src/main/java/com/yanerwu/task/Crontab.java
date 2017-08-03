e4b31a956a05111fa79388659dc633f94753818f
zhi_heart <zhi_heart@aliyun.com>
2017-08-03 16:47:56 +0800

    优化

--- collect/src/main/java/com/yanerwu/task/Crontab.java
+++ collect/src/main/java/com/yanerwu/task/Crontab.java
@@ -9,6 +9,8 @@ import com.yanerwu.entity.MvList;
 import com.yanerwu.pipeline.YsdqPipeline;
 import com.yanerwu.processor.YsdqProcessor;
 import com.yanerwu.service.BiqugeService;
+import com.yanerwu.utils.HttpClientUtil;
+import com.yanerwu.utils.Mail;
 import org.apache.commons.dbutils.BasicRowProcessor;
 import org.apache.commons.dbutils.GenerousBeanProcessor;
 import org.apache.commons.dbutils.RowProcessor;
@@ -23,6 +25,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @Author Zuz
@@ -87,5 +91,39 @@ public class Crontab {
         biqugeService.biqugeDetail(9999);
     }
 
+    @Scheduled(cron = "8 8 15 ? * 2,3,4,5,6 *")
+    public void stock(){
+        String result = "";
+        try {
+            double amount = 0;
+            Pattern pattern = Pattern.compile(".*=\"(.*)\";");
+            Matcher matcher = pattern.matcher(HttpClientUtil.doGet("http://hq.sinajs.cn/list=sz002230,sz300033"));
+            while (matcher.find()) {
+                String[] stock = matcher.group(1).split(",");
+                String name = stock[0];
+                Double price = Double.valueOf(stock[3]);
+                switch (name) {
+                    case "科大讯飞":
+                        amount += price * 3000;
+                        break;
+                    case "同花顺":
+                        amount += price * 1900;
+                        break;
+                }
+            }
+
+            result = String.format("--------------</br>%.2f&nbsp;&nbsp;&nbsp;&nbsp;%.2f%%</br>--------------<br>", amount / 10000, ((amount / 188152) - 1) * 100);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        String subject = "账户详情";
+        String content = result;
+        String smtp = "smtp.163.com";
+        String from = "zhi_heart@163.com";
+        String to = "zhi_heart@aliyun.com";
+        String username = "zhi_heart@163.com";
+        String password = "3kNu8ZTzHFGWkE";
+        Mail.sendAndCc(smtp, from, to, null, subject, content, username, password);
+    }
 
 }
