f4e8fe52f4e4336331fa3c459e701770ab5c3011
zhi_heart <zhi_heart@aliyun.com>
2017-09-11 16:10:01 +0800

    优化

--- collect/src/main/java/com/yanerwu/task/Crontab.java
+++ collect/src/main/java/com/yanerwu/task/Crontab.java
@@ -3,6 +3,7 @@ package com.yanerwu.task;
 
 import com.yanerwu.common.DbUtilsTemplate;
 import com.yanerwu.service.BiqugeService;
+import com.yanerwu.utils.DateUtils;
 import com.yanerwu.utils.HttpClientUtil;
 import com.yanerwu.utils.Mail;
 import org.slf4j.Logger;
@@ -78,36 +79,57 @@ public class Crontab {
 
     @Scheduled(cron = "8 8 15 ? * 1,2,3,4,5")
     public void stock() {
-        double c=320000.0;
-        String subject = "", content = "";
+        double totalAmount = 320000.0;
+        int kdxf = 256343;
+        int ths = 63657;
+        String subject = "";
+        StringBuffer content = new StringBuffer();
         try {
             double nowAmount = 0, lastAmount = 0;
             Pattern pattern = Pattern.compile(".*=\"(.*)\";");
             Matcher matcher = pattern.matcher(HttpClientUtil.doGet("http://hq.sinajs.cn/list=sz002230,sz300033"));
             while (matcher.find()) {
                 String[] stock = matcher.group(1).split(",");
+                if(!DateUtils.getBackDate(0).equals(stock[stock.length - 3])){
+                    return;
+                }
                 String name = stock[0];
                 Double nowPrice = Double.valueOf(stock[3]);
                 Double lastPrice = Double.valueOf(stock[2]);
 
+                int dayAmount = 0;
+                int amount = 0;
                 switch (name) {
                     case "科大讯飞":
                         nowAmount += nowPrice * 5000;
                         lastAmount += lastPrice * 5000;
+                        dayAmount = (int) ((nowPrice - lastPrice) * 5000);
+                        amount = (int) (nowPrice * 5000) - kdxf;
                         break;
                     case "同花顺":
                         nowAmount += nowPrice * 1000;
                         lastAmount += lastPrice * 1000;
+                        dayAmount = (int) ((nowPrice - lastPrice) * 1000);
+                        amount = (int) (nowPrice * 1000) - ths;
                         break;
                 }
+
+                String c = String.format("%s -> %s %3.2f%% %s -> %+-5d -> %+-6d",
+                        "同花顺".equals(name) ? "T" : "K",
+                        nowPrice >= lastPrice ? "↑" : "↓",
+                        (nowPrice - lastPrice) / lastPrice * 100,
+                        nowPrice >= lastPrice ? "↑" : "↓",
+                        dayAmount,
+                        amount);
+                content.append(String.format("<p>%s</p>\n", c));
             }
 
             subject = String.format("%s %.2f%% %s -> %s -> %s",
                     nowAmount >= lastAmount ? "↑" : "↓",
-                    ((nowAmount / c) - 1) * 100,
+                    ((nowAmount / totalAmount) - 1) * 100,
                     nowAmount >= lastAmount ? "↑" : "↓",
                     (int) (nowAmount - lastAmount),
-                    (int) (nowAmount - c));
+                    (int) (nowAmount - totalAmount));
         } catch (NumberFormatException e) {
             e.printStackTrace();
         }
@@ -117,7 +139,7 @@ public class Crontab {
         String to = "zhi_heart@aliyun.com";
         String username = "zhi_heart@163.com";
         String password = "3kNu8ZTzHFGWkE";
-        Mail.sendAndCc(smtp, from, to, null, subject, content, username, password);
+        Mail.sendAndCc(smtp, from, to, null, subject, content.toString().replace(" ","&nbsp;"), username, password);
     }
 
 }
